71d7fab9f3994411999498b7af23dc2f9ef13860
Casey Mershon <caseymershon@gmail.com>
2023-08-09 15:45:27 -0400

    implement web client authentication

---
+++ src/app/api/auth/session/route.ts
@@ -0,0 +1,16 @@
+import { getServerSession } from 'next-auth/next'
+import { NextResponse } from 'next/server'
+import { authOptions } from '../[...nextauth]/route'
+
+
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return new NextResponse(JSON.stringify({ error: 'unauthorized' }), {
+      status: 401
+    })
+  }
+
+  return NextResponse.json({ authenticated: !!session })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-import Image from 'next/image'
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { User } from "./user";
+import { LoginButton, LogoutButton } from "@/components/buttons.components";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      </div>
+    <main>
+      <LoginButton />
+      <LogoutButton />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className={`mb-3 text-2xl font-semibold`}>
-          Holdfast Roleplay Microservice
-        </h1>
-      </div>
+      <h2>Server Session</h2>
+      <pre>{JSON.stringify(session)}</pre>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      </div>
+      <h2>Client Session</h2>
+      <User />
     </main>
-  )
+  );
 }
